ca64caafb63806808951344a3d222c05e39db627
Felix Prinz <felix.prinz@gintel.com>
2025-07-08 13:02:58 +0200

    allow for different number of dots on rune sides

--- runeWorklet.js
+++ runeWorklet.js
@@ -6,11 +6,13 @@ class RuneSmith {
   sides;
   points;
   nodes;
+  dots;
   runeValue;
 
   parseRune(runeValue) {
     this.nodes = [];
     this.sides = 0;
+    this.dots = [];
     let i = 1;
     [
       127, // all connections to 1
@@ -24,10 +26,12 @@ class RuneSmith {
     ].forEach((nodeConnections) => {
       if ((runeValue & nodeConnections) !== 0) {
         this.nodes.push(i);
+        this.dots.push(0);
         i++;
         this.sides++;
       } else {
         this.nodes.push(false);
+        this.dots[this.dots.length - 1]++;
       }
     });
   }
@@ -36,14 +40,16 @@ class RuneSmith {
     // Use `ctx` as if it was a normal canvas
 
     this.runeValue = props.get("--rune-value").value;
-    this.parseRune(this.runeValue); // TODO remove check
+    this.parseRune(this.runeValue);
 
     this.generatePoints(geom, this.sides || 3 + Math.floor(Math.random() * 3));
 
     ctx.strokeStyle = "black";
     ctx.lineWidth = 2;
 
-    this.drawLines(ctx); // TODO remove check
+    this.drawLines(ctx);
+
+    this.drawDots(ctx, geom);
   }
 
   generatePoints(geom, n) {
@@ -51,20 +57,11 @@ class RuneSmith {
     for (let i = 1; i <= n; i++) {
       this.points.push({
         x:
-          geom.width * 0.5 + geom.width * 0.4 * Math.sin((2 * Math.PI * i) / n),
+          geom.width * 0.5 + geom.width * 0.3 * Math.sin((2 * Math.PI * i) / n),
         y:
           geom.height * 0.5 +
-          geom.height * 0.4 * Math.cos((2 * Math.PI * i) / n),
+          geom.height * 0.3 * Math.cos((2 * Math.PI * i) / n),
       });
-    }
-  }
-
-  drawNGon(ctx) {
-    for (const point of this.points) {
-      ctx.beginPath();
-      ctx.lineTo(point.x, point.y);
-      ctx.closePath();
-      ctx.stroke();
     }
   }
 
@@ -79,13 +76,160 @@ class RuneSmith {
           ctx.beginPath();
           ctx.moveTo(startPoint.x, startPoint.y);
           ctx.lineTo(endPoint.x, endPoint.y);
-          ctx.closePath();
           ctx.stroke();
         }
 
         i++;
       }
     }
+  }
+
+  drawDots(ctx, geom) {
+    if (this.sides === 0) {
+      this.generatePoints(geom, 8);
+      for (let point of this.points) {
+        this.renderDot(ctx, point);
+      }
+      return;
+    }
+
+    for (let a = 0; a < this.dots.length; a++) {
+      if (!!this.dots[a]) {
+        this.renderDots(ctx, geom, a, this.dots[a]);
+      }
+    }
+  }
+
+  renderDots(ctx, geom, sideNumber, dotNumber) {
+    const startPoint = this.points[sideNumber];
+    const endPoint = this.points[(sideNumber + 1) % this.points.length];
+    const midPoint = {
+      x: geom.width * 0.5,
+      y: geom.height * 0.5,
+    };
+    const sideMidPoint = {
+      x: (startPoint.x + endPoint.x) / 2,
+      y: (startPoint.y + endPoint.y) / 2,
+    };
+
+    let factor = 0.3;
+    if (this.sides === 3) {
+      factor = 0.4;
+    }
+
+    let verticalOffset = {
+      x: factor * (sideMidPoint.x - midPoint.x),
+      y: factor * (sideMidPoint.y - midPoint.y),
+    };
+    if (this.sides === 2) {
+      verticalOffset = {
+        x: factor * (startPoint.y - midPoint.x),
+        y: factor * (startPoint.x - midPoint.y),
+      };
+    }
+
+    let horizontalOffset = {
+      x: factor * (startPoint.x - endPoint.x),
+      y: factor * (startPoint.y - endPoint.y),
+    };
+
+    if (dotNumber === 1) {
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x,
+        y: sideMidPoint.y + verticalOffset.y,
+      });
+    } else if (dotNumber === 2) {
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x + horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y + horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x - horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y - horizontalOffset.y / 2,
+      });
+    } else if (dotNumber === 3) {
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x + horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y + horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x - horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y - horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 2,
+        y: sideMidPoint.y + verticalOffset.y * 2,
+      });
+    } else if (dotNumber === 4) {
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x + horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y + horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x - horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y - horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 2 + horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y * 2 + horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 2 - horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y * 2 - horizontalOffset.y / 2,
+      });
+    } else if (dotNumber === 5) {
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x,
+        y: sideMidPoint.y + verticalOffset.y,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x + horizontalOffset.x,
+        y: sideMidPoint.y + verticalOffset.y + horizontalOffset.y,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x - horizontalOffset.x,
+        y: sideMidPoint.y + verticalOffset.y - horizontalOffset.y,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 2 + horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y * 2 + horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 2 - horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y * 2 - horizontalOffset.y / 2,
+      });
+    } else if (dotNumber === 6) {
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x,
+        y: sideMidPoint.y + verticalOffset.y,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x + horizontalOffset.x,
+        y: sideMidPoint.y + verticalOffset.y + horizontalOffset.y,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x - horizontalOffset.x,
+        y: sideMidPoint.y + verticalOffset.y - horizontalOffset.y,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 2 + horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y * 2 + horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 2 - horizontalOffset.x / 2,
+        y: sideMidPoint.y + verticalOffset.y * 2 - horizontalOffset.y / 2,
+      });
+      this.renderDot(ctx, {
+        x: sideMidPoint.x + verticalOffset.x * 3,
+        y: sideMidPoint.y + verticalOffset.y * 3,
+      });
+    }
+  }
+
+  renderDot(ctx, point) {
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI, true);
+    ctx.stroke();
   }
 }
 
